be573237c8fd1e42de29bc624aeabb07707e55ca
user <user@DESKTOP-MABBG6D>
2026-03-19 10:20:50 +0900

    과제1 - 기본 UI 및 데이터 연동 1차 커밋 1.UI 구성 ( Label(제목), TextBox(입력), Button(전송), ListBox(대화창) ) 배치. 2.전송 기능 구현(버튼 클릭시 textbox 내용을 listbox로 옮기고 textbox 내용 삭제). 3.부가 기능 - textbox나 버튼에 마우스 올릴 시 포인트 변경, 폼 텍스트(제목) 변경

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            MessageList = new ListBox();
+            EnterMessage = new TextBox();
+            btnEnter = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("굴림체", 48F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label1.ForeColor = Color.Blue;
+            label1.Location = new Point(47, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(385, 64);
+            label1.TabIndex = 0;
+            label1.Text = "에코 메신저";
+            label1.Click += label1_Click;
+            // 
+            // MessageList
+            // 
+            MessageList.Font = new Font("한컴 고딕", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            MessageList.FormattingEnabled = true;
+            MessageList.Location = new Point(47, 100);
+            MessageList.Name = "MessageList";
+            MessageList.Size = new Size(683, 221);
+            MessageList.TabIndex = 1;
+            MessageList.SelectedIndexChanged += MessageList_SelectedIndexChanged;
+            MessageList.DataContextChanged += MessageList_DataContextChanged;
+            // 
+            // EnterMessage
+            // 
+            EnterMessage.Cursor = Cursors.IBeam;
+            EnterMessage.Font = new Font("맑은 고딕", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            EnterMessage.Location = new Point(47, 357);
+            EnterMessage.Name = "EnterMessage";
+            EnterMessage.Size = new Size(492, 33);
+            EnterMessage.TabIndex = 2;
+            // 
+            // btnEnter
+            // 
+            btnEnter.BackColor = Color.Cyan;
+            btnEnter.Cursor = Cursors.Hand;
+            btnEnter.Font = new Font("한컴 고딕", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btnEnter.Location = new Point(567, 339);
+            btnEnter.Name = "btnEnter";
+            btnEnter.Size = new Size(163, 65);
+            btnEnter.TabIndex = 3;
+            btnEnter.Text = "전 송";
+            btnEnter.UseVisualStyleBackColor = false;
+            btnEnter.Click += btnEnter_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 255, 192);
             ClientSize = new Size(800, 450);
-            Text = "Form1";
+            Controls.Add(btnEnter);
+            Controls.Add(EnterMessage);
+            Controls.Add(MessageList);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Echo Messenger";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private ListBox MessageList;
+        private TextBox EnterMessage;
+        private Button btnEnter;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,38 @@
 namespace EchoMessenger
 {
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //버튼 클릭 이벤트 핸들러 추가
+        // - 입력 상자에서 텍스트를 읽고
+        // - 메시지 리스트에 추가한 뒤
+        // - 입력 상자를 비운다
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            string message = EnterMessage.Text;
+
+            MessageList.Items.Add(message);
+            EnterMessage.Clear();
+        }
+
+        private void MessageList_DataContextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MessageList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
